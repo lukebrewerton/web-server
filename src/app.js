bb4eec6aab5ce25b93e589fb42304f4a988b7728
Luke Brewerton <luke@brewerton.me>
2019-04-25 23:09:53 +0100

    :lipstick: Started Handlebars

--- src/app.js
+++ src/app.js
@@ -1,21 +1,40 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
-app.get('', (req, res) => {
-  res.send('Hello express!');
-});
+const publicDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
 
-app.get('/help', (req, res) => {
-  res.send('Help page');
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+app.use(express.static(publicDirPath));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Weather App Homepage'
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About');
+  res.render('about', {
+    title: 'About Me',
+    name: 'Luke Brewerton'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help!',
+    helpText: 'This is a help message!'
+  });
 });
 
 app.get('/weather', (req, res) => {
-  res.send('Your weather');
+  res.send({
+    forcast: 'It is sunny',
+    location: 'Brighton'
+  });
 });
 
 app.listen(3000, () => {
